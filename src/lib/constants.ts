313d9d4b465205a8904dfd8b7ebe4a3a6c42f6cc
Kinsu Rajput <kissuxrajput@gmail.com>
2025-09-13 14:39:11 +0530

    Create constants.ts

---
+++ src/lib/constants.ts
@@ -0,0 +1,30 @@
+type NavLink = {
+  name: string;
+  href: string;
+};
+
+export const siteLinks: NavLink[] = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "All Lessons",
+    href: "/Lessons",
+  },
+  {
+    name: "About Us",
+    href: "/About",
+  },
+];
+
+export const externalLinks: NavLink[] = [
+  {
+    name: "Star Repository",
+    href: "https://github.com/pykinsu/Termux-HandBook",
+  },
+  {
+    name: "Creator",
+    href: "https://github.com/pykinsu/",
+  },
+];
