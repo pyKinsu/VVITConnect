14964b10bb0c3d71f2d3b682edd2b9e31e4e8526
Kinsu Rajput <kissuxrajput@gmail.com>
2025-09-13 18:48:22 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <div className="flex flex-col py-8">
       <h1 className="text-balance mb-4 text-center text-4xl font-extrabold">
-        Choose Your Branche
+        Choose Your Branch Below
       </h1>
 
       <Branches />
