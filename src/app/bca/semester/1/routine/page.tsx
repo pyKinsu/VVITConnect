59756046dc7ef1cd5c909a4149017a439ee62999
Kinsu Rajput <kissuxrajput@gmail.com>
2025-09-15 08:56:11 +0530

    Update page.tsx

--- src/app/bca/semester/1/routine/page.tsx
+++ src/app/bca/semester/1/routine/page.tsx
@@ -1,201 +1,224 @@
 "use client";
-import { useState, useEffect } from "react";
-import { FiAlertCircle, FiChevronLeft, FiChevronRight, FiMapPin } from "react-icons/fi";
 
-type Period = {
+import React, { useState, useEffect } from "react";
+import { FaInfoCircle } from "react-icons/fa";
+import { BsPinAngleFill } from "react-icons/bs";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+
+type RoutineEntry = {
   time: string;
   subject: string;
   fullForm: string;
 };
 
 type DayRoutine = {
-  day: string;
-  periods: Period[];
+  [day: string]: RoutineEntry[];
 };
 
-// Routine data (example)
-const routineA: DayRoutine[]  = {
+// ---------- ROUTINE A ----------
+const routineA: DayRoutine = {
   Monday: [
-    { time: "10:00 - 10:50 AM", subject: "1BCA PMO", fullForm: "Principles of Management" },
-    { time: "11:00 - 11:50 AM", subject: "1BCA ITA", fullForm: "Introduction to Accounting" },
-    { time: "12:00 - 12:50 PM", subject: "1BCA ITA Lab", fullForm: "Accounting Lab" },
-    { time: "1:00 - 1:50 PM", subject: "1BCA PMO", fullForm: "Principles of Management" },
+    { time: "10:00 - 10:50 AM", subject: "PMO", fullForm: "Principles of Management" },
+    { time: "11:00 - 11:50 AM", subject: "ITA", fullForm: "Introduction to Accounting" },
+    { time: "12:00 - 12:50 PM", subject: "ITA Lab", fullForm: "Accounting Lab" },
+    { time: "1:00 - 1:50 PM", subject: "PMO", fullForm: "Principles of Management" },
   ],
   Tuesday: [
-    { time: "10:00 - 10:50 AM", subject: "1BCA CE", fullForm: "Computer Essentials" },
-    { time: "11:00 - 11:50 AM", subject: "1BCA ITA", fullForm: "Introduction to Accounting" },
-    { time: "12:00 - 12:50 PM", subject: "1BCA PMO", fullForm: "Principles of Management" },
-    { time: "1:00 - 1:50 PM", subject: "1BCA CE", fullForm: "Computer Essentials" },
-    { time: "2:00 - 2:50 PM", subject: "1BCA PSPC", fullForm: "Problem Solving & Programming Concepts" },
+    { time: "10:00 - 10:50 AM", subject: "CE", fullForm: "Computer Essentials" },
+    { time: "11:00 - 11:50 AM", subject: "ITA", fullForm: "Introduction to Accounting" },
+    { time: "12:00 - 12:50 PM", subject: "PMO", fullForm: "Principles of Management" },
+    { time: "1:00 - 1:50 PM", subject: "CE", fullForm: "Computer Essentials" },
+    { time: "2:00 - 2:50 PM", subject: "PSPC", fullForm: "Problem Solving & Programming Concepts" },
   ],
   Wednesday: [
-    { time: "10:00 - 10:50 AM", subject: "1BCA PMO", fullForm: "Principles of Management" },
-    { time: "11:00 - 11:50 AM", subject: "1BCA CE", fullForm: "Computer Essentials" },
-    { time: "12:00 - 12:50 PM", subject: "1BCA BM", fullForm: "Business Mathematics" },
-    { time: "1:00 - 1:50 PM", subject: "1BCA ITA", fullForm: "Introduction to Accounting" },
-    { time: "2:00 - 2:50 PM", subject: "1BCA PMO", fullForm: "Principles of Management" },
+    { time: "10:00 - 10:50 AM", subject: "PMO", fullForm: "Principles of Management" },
+    { time: "11:00 - 11:50 AM", subject: "CE", fullForm: "Computer Essentials" },
+    { time: "12:00 - 12:50 PM", subject: "BM", fullForm: "Business Mathematics" },
+    { time: "1:00 - 1:50 PM", subject: "ITA", fullForm: "Introduction to Accounting" },
+    { time: "2:00 - 2:50 PM", subject: "PMO", fullForm: "Principles of Management" },
   ],
   Thursday: [
-    { time: "10:00 - 10:50 AM", subject: "1BCA PSPC Lab", fullForm: "Problem Solving Lab" },
-    { time: "11:00 - 11:50 AM", subject: "1BCA ITA", fullForm: "Introduction to Accounting" },
-    { time: "12:00 - 12:50 PM", subject: "1BCA PSPC", fullForm: "Problem Solving & Programming Concepts" },
-    { time: "1:00 - 1:50 PM", subject: "1BCA ITA", fullForm: "Introduction to Accounting" },
+    { time: "10:00 - 10:50 AM", subject: "PSPC Lab", fullForm: "Problem Solving Lab" },
+    { time: "11:00 - 11:50 AM", subject: "ITA", fullForm: "Introduction to Accounting" },
+    { time: "12:00 - 12:50 PM", subject: "PSPC", fullForm: "Problem Solving & Programming Concepts" },
+    { time: "1:00 - 1:50 PM", subject: "ITA", fullForm: "Introduction to Accounting" },
   ],
   Friday: [
-    { time: "10:00 - 10:50 AM", subject: "1BCA ITA", fullForm: "Introduction to Accounting" },
-    { time: "11:00 - 11:50 AM", subject: "1BCA BM", fullForm: "Business Mathematics" },
-    { time: "12:00 - 12:50 PM", subject: "1BCA PSPC", fullForm: "Problem Solving & Programming Concepts" },
-    { time: "1:00 - 1:50 PM", subject: "1BCA PSPC Lab", fullForm: "Problem Solving Lab" },
+    { time: "10:00 - 10:50 AM", subject: "ITA", fullForm: "Introduction to Accounting" },
+    { time: "11:00 - 11:50 AM", subject: "BM", fullForm: "Business Mathematics" },
+    { time: "12:00 - 12:50 PM", subject: "PSPC", fullForm: "Problem Solving & Programming Concepts" },
+    { time: "1:00 - 1:50 PM", subject: "PSPC Lab", fullForm: "Problem Solving Lab" },
     { time: "2:00 - 2:50 PM", subject: "Spt & Cul", fullForm: "Sports & Cultural Activities" },
   ],
   Saturday: [
-    { time: "10:00 - 10:50 AM", subject: "1BCA BM", fullForm: "Business Mathematics" },
-    { time: "11:00 - 11:50 AM", subject: "1BCA PSPC", fullForm: "Problem Solving & Programming Concepts" },
-    { time: "12:00 - 12:50 PM", subject: "1BCA ITA", fullForm: "Introduction to Accounting" },
+    { time: "10:00 - 10:50 AM", subject: "BM", fullForm: "Business Mathematics" },
+    { time: "11:00 - 11:50 AM", subject: "PSPC", fullForm: "Problem Solving & Programming Concepts" },
+    { time: "12:00 - 12:50 PM", subject: "ITA", fullForm: "Introduction to Accounting" },
   ],
 };
 
-const routineB: DayRoutine[] = {
+// ---------- ROUTINE B ----------
+const routineB: DayRoutine = {
   Monday: [
-    { time: "10:00 - 10:50 AM", subject: "1BCA ITA", fullForm: "Introduction to Accounting" },
-    { time: "11:00 - 11:50 AM", subject: "1BCA PSPC", fullForm: "Problem Solving & Programming Concepts" },
-    { time: "12:00 - 12:50 PM", subject: "1BCA CE", fullForm: "Computer Essentials" },
-    { time: "1:00 - 1:50 PM", subject: "1BCA PMO", fullForm: "Principles of Management" },
+    { time: "10:00 - 10:50 AM", subject: "ITA", fullForm: "Introduction to Accounting" },
+    { time: "11:00 - 11:50 AM", subject: "PSPC", fullForm: "Problem Solving & Programming Concepts" },
+    { time: "12:00 - 12:50 PM", subject: "CE", fullForm: "Computer Essentials" },
+    { time: "1:00 - 1:50 PM", subject: "PMO", fullForm: "Principles of Management" },
   ],
   Tuesday: [
-    { time: "10:00 - 10:50 AM", subject: "1BCA PSPC", fullForm: "Problem Solving & Programming Concepts" },
-    { time: "11:00 - 11:50 AM", subject: "1BCA PSPC Lab", fullForm: "Problem Solving Lab" },
-    { time: "12:00 - 12:50 PM", subject: "1BCA PMO", fullForm: "Principles of Management" },
-    { time: "1:00 - 1:50 PM", subject: "1BCA ITA", fullForm: "Introduction to Accounting" },
+    { time: "10:00 - 10:50 AM", subject: "PSPC", fullForm: "Problem Solving & Programming Concepts" },
+    { time: "11:00 - 11:50 AM", subject: "PSPC Lab", fullForm: "Problem Solving Lab" },
+    { time: "12:00 - 12:50 PM", subject: "PMO", fullForm: "Principles of Management" },
+    { time: "1:00 - 1:50 PM", subject: "ITA", fullForm: "Introduction to Accounting" },
   ],
   Wednesday: [
-    { time: "10:00 - 10:50 AM", subject: "1BCA CE", fullForm: "Computer Essentials" },
-    { time: "11:00 - 11:50 AM", subject: "1BCA BM", fullForm: "Business Mathematics" },
-    { time: "12:00 - 12:50 PM", subject: "1BCA PMO", fullForm: "Principles of Management" },
-    { time: "1:00 - 1:50 PM", subject: "1BCA PSPC", fullForm: "Problem Solving & Programming Concepts" },
-    { time: "2:00 - 2:50 PM", subject: "1BCA ITA", fullForm: "Introduction to Accounting" },
+    { time: "10:00 - 10:50 AM", subject: "CE", fullForm: "Computer Essentials" },
+    { time: "11:00 - 11:50 AM", subject: "BM", fullForm: "Business Mathematics" },
+    { time: "12:00 - 12:50 PM", subject: "PMO", fullForm: "Principles of Management" },
+    { time: "1:00 - 1:50 PM", subject: "PSPC", fullForm: "Problem Solving & Programming Concepts" },
+    { time: "2:00 - 2:50 PM", subject: "ITA", fullForm: "Introduction to Accounting" },
   ],
   Thursday: [
-    { time: "10:00 - 10:50 AM", subject: "1BCA BM", fullForm: "Business Mathematics" },
-    { time: "11:00 - 11:50 AM", subject: "1BCA ITA", fullForm: "Introduction to Accounting" },
-    { time: "12:00 - 12:50 PM", subject: "1BCA PMO", fullForm: "Principles of Management" },
-    { time: "1:00 - 1:50 PM", subject: "1BCA CE", fullForm: "Computer Essentials" },
-    { time: "2:00 - 2:50 PM", subject: "1BCA PSPC", fullForm: "Problem Solving & Programming Concepts" },
+    { time: "10:00 - 10:50 AM", subject: "BM", fullForm: "Business Mathematics" },
+    { time: "11:00 - 11:50 AM", subject: "ITA", fullForm: "Introduction to Accounting" },
+    { time: "12:00 - 12:50 PM", subject: "PMO", fullForm: "Principles of Management" },
+    { time: "1:00 - 1:50 PM", subject: "CE", fullForm: "Computer Essentials" },
+    { time: "2:00 - 2:50 PM", subject: "PSPC", fullForm: "Problem Solving & Programming Concepts" },
   ],
   Friday: [
-    { time: "10:00 - 10:50 AM", subject: "1BCA ITA", fullForm: "Introduction to Accounting" },
-    { time: "11:00 - 11:50 AM", subject: "1BCA ITA", fullForm: "Introduction to Accounting" },
-    { time: "12:00 - 12:50 PM", subject: "1BCA CE", fullForm: "Computer Essentials" },
-    { time: "1:00 - 1:50 PM", subject: "1BCA PSPC", fullForm: "Problem Solving & Programming Concepts" },
+    { time: "10:00 - 10:50 AM", subject: "ITA", fullForm: "Introduction to Accounting" },
+    { time: "11:00 - 11:50 AM", subject: "ITA", fullForm: "Introduction to Accounting" },
+    { time: "12:00 - 12:50 PM", subject: "CE", fullForm: "Computer Essentials" },
+    { time: "1:00 - 1:50 PM", subject: "PSPC", fullForm: "Problem Solving & Programming Concepts" },
   ],
   Saturday: [
-    { time: "10:00 - 10:50 AM", subject: "1BCA ITA", fullForm: "Introduction to Accounting" },
-    { time: "11:00 - 11:50 AM", subject: "1BCA ITA Lab", fullForm: "Accounting Lab" },
-    { time: "12:00 - 12:50 PM", subject: "1BCA BM", fullForm: "Business Mathematics" },
+    { time: "10:00 - 10:50 AM", subject: "ITA", fullForm: "Introduction to Accounting" },
+    { time: "11:00 - 11:50 AM", subject: "ITA Lab", fullForm: "Accounting Lab" },
+    { time: "12:00 - 12:50 PM", subject: "BM", fullForm: "Business Mathematics" },
     { time: "1:00 - 1:50 PM", subject: "Spt & Cul", fullForm: "Sports & Cultural Activities" },
   ],
 };
 
+// ---------- MAIN COMPONENT ----------
 export default function RoutinePage() {
-  const [dayIndex, setDayIndex] = useState(new Date().getDay() - 1); // Mon=0
-  const [infoText, setInfoText] = useState<string | null>(null);
-  const [currentPeriod, setCurrentPeriod] = useState<string | null>(null);
-  const [nextInfo, setNextInfo] = useState<string>("");
+  const [section, setSection] = useState<"A" | "B">("A");
+  const [dayIndex, setDayIndex] = useState(new Date().getDay() - 1); // 0=Monday
+  const [notification, setNotification] = useState<string | null>(null);
+  const [currentClass, setCurrentClass] = useState<string | null>(null);
 
-  const routine = routineA[dayIndex >= 0 && dayIndex < routineA.length ? dayIndex : 0];
+  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const routines = section === "A" ? routineA : routineB;
 
-  // track time and set current/next
+  // --- highlight current class ---
   useEffect(() => {
-    const updateClassStatus = () => {
+    const checkCurrent = () => {
       const now = new Date();
-      const nowMinutes = now.getHours() * 60 + now.getMinutes();
+      const today = days[dayIndex] || "Monday";
+      const todayRoutine = routines[today] || [];
+      const current = todayRoutine.find((entry) => {
+        const [start, end] = entry.time.split(" - ");
+        const startTime = new Date(`${now.toDateString()} ${start}`);
+        const endTime = new Date(`${now.toDateString()} ${end}`);
+        return now >= startTime && now <= endTime;
+      });
 
-      let active = null;
-      let upcoming = null;
-
-      for (let i = 0; i < routine.periods.length; i++) {
-        const [sh, sm] = routine.periods[i].time.split(" - ")[0].split(":").map(Number);
-        const [eh, em] = routine.periods[i].time.split(" - ")[1].split(":").map(Number);
-
-        const startM = sh * 60 + sm;
-        const endM = eh * 60 + em;
-
-        if (nowMinutes >= startM && nowMinutes <= endM) {
-          active = routine.periods[i].subject;
-          break;
-        } else if (nowMinutes < startM && !upcoming) {
-          upcoming = `Upcoming: ${routine.periods[i].subject} at ${routine.periods[i].time}`;
-        }
+      if (current) {
+        setCurrentClass(current.subject);
+      } else {
+        const upcoming = todayRoutine.find((entry) => {
+          const [start] = entry.time.split(" - ");
+          const startTime = new Date(`${now.toDateString()} ${start}`);
+          return now < startTime;
+        });
+        setCurrentClass(upcoming ? `Upcoming: ${upcoming.subject}` : "Classes Over");
       }
-
-      if (active) setCurrentPeriod(active);
-      else if (upcoming) setNextInfo(upcoming);
-      else setNextInfo("All classes over");
     };
 
-    updateClassStatus();
-    const timer = setInterval(updateClassStatus, 60000);
-    return () => clearInterval(timer);
-  }, [routine]);
+    checkCurrent();
+    const interval = setInterval(checkCurrent, 60000);
+    return () => clearInterval(interval);
+  }, [dayIndex, section]);
+
+  const today = days[dayIndex] || "Monday";
+  const todayRoutine = routines[today] || [];
 
   return (
-    <div className="flex flex-col items-center p-4 space-y-4">
-      {/* Day navigation */}
-      <div className="flex items-center space-x-4">
-        <button onClick={() => setDayIndex((dayIndex - 1 + routineA.length) % routineA.length)}>
-          <FiChevronLeft size={28} />
+    <main className="flex flex-col items-center px-4 py-6 min-h-[85vh]">
+      {/* Section Toggle */}
+      <div className="flex gap-4 mb-4">
+        <button
+          onClick={() => setSection("A")}
+          className={`px-4 py-2 rounded-lg font-semibold ${
+            section === "A" ? "bg-purple-500 text-white" : "bg-gray-200"
+          }`}
+        >
+          BCA - A
         </button>
-        <h1 className="text-2xl font-bold">{routine.day}</h1>
-        <button onClick={() => setDayIndex((dayIndex + 1) % routineA.length)}>
-          <FiChevronRight size={28} />
+        <button
+          onClick={() => setSection("B")}
+          className={`px-4 py-2 rounded-lg font-semibold ${
+            section === "B" ? "bg-purple-500 text-white" : "bg-gray-200"
+          }`}
+        >
+          BCA - B
         </button>
       </div>
 
-      {/* Class periods */}
-      <div className="w-full max-w-3xl space-y-3">
-        {routine.periods.map((period) => (
-          <div
-            key={period.time + period.subject}
-            className={`flex items-center justify-between py-4 px-6 rounded-xl shadow-md
-                        ${currentPeriod === period.subject ? "bg-yellow-100 border-l-4 border-yellow-500" : "bg-white/80"}`}
+      {/* Day Navigation */}
+      <div className="flex items-center gap-4 mb-4">
+        <button
+          onClick={() => setDayIndex((dayIndex - 1 + days.length) % days.length)}
+        >
+          <MdChevronLeft size={28} />
+        </button>
+        <h2 className="text-xl font-bold">{today}</h2>
+        <button onClick={() => setDayIndex((dayIndex + 1) % days.length)}>
+          <MdChevronRight size={28} />
+        </button>
+      </div>
+
+      {/* Current Class Info */}
+      <div className="text-lg font-semibold text-purple-600 mb-4 flex items-center gap-2">
+        <BsPinAngleFill /> {currentClass}
+      </div>
+
+      {/* Routine List */}
+      <ul className="w-full max-w-xl space-y-3">
+        {todayRoutine.map((entry, idx) => (
+          <li
+            key={idx}
+            className={`flex justify-between items-center p-3 rounded-lg ${
+              currentClass?.includes(entry.subject)
+                ? "bg-yellow-100 border border-yellow-400"
+                : "bg-transparent"
+            }`}
           >
             <div>
-              <div className="font-semibold text-lg flex items-center gap-2">
-                {period.subject}
-                {currentPeriod === period.subject && <FiMapPin className="text-yellow-500" />}
-              </div>
-              <div className="text-sm text-gray-600">{period.time}</div>
+              <p className="font-semibold">{entry.subject}</p>
+              <p className="text-sm text-gray-500">{entry.time}</p>
             </div>
-            <FiAlertCircle
-              className="text-yellow-500 cursor-pointer hover:scale-110 transition"
-              size={22}
-              onClick={() => setInfoText(period.fullForm)}
-            />
-          </div>
+            <button
+              onClick={() => setNotification(entry.fullForm)}
+              className="text-yellow-500"
+            >
+              <FaInfoCircle size={20} />
+            </button>
+          </li>
         ))}
-      </div>
+      </ul>
 
-      {/* Upcoming / Over message */}
-      <div className="text-center text-gray-700 font-medium mt-4">
-        {nextInfo && <p>{nextInfo}</p>}
-      </div>
-
-      {/* Global Info Notification */}
-      {infoText && (
-        <div
-          className="fixed bottom-8 left-1/2 -translate-x-1/2
-                     bg-yellow-200 text-yellow-900 px-6 py-3 rounded-xl shadow-xl
-                     font-medium animate-fadeIn z-[9999]"
-        >
-          {infoText}
+      {/* Notification */}
+      {notification && (
+        <div className="fixed bottom-20 bg-yellow-200 text-yellow-900 px-4 py-2 rounded-lg shadow-md">
+          {notification}
           <button
-            onClick={() => setInfoText(null)}
-            className="ml-4 text-sm underline text-yellow-900"
+            onClick={() => setNotification(null)}
+            className="ml-3 text-red-600 font-bold"
           >
-            Close
+            ✕
           </button>
         </div>
       )}
-    </div>
+    </main>
   );
 }
