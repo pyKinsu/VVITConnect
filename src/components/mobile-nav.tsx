2374374d3cf8bf6e164799a1ea5d8b46cbf4669b
Kinsu Rajput <kissuxrajput@gmail.com>
2025-09-15 20:48:38 +0530

    Update mobile-nav.tsx

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -17,18 +17,9 @@ export function MobileNav({ className }: { className?: string }) {
   const handleCloseSheet = () => setOpen(false);
 
   const socials = [
-    {
-      name: "GitHub",
-      href: "https://github.com/pykinsu",
-    },
-    {
-      name: "Community",
-      href: "https://t.me/VVITConnect",
-    },
-    {
-      name: "Twitter",
-      href: "https://twitter.com/pykinsu",
-    },
+    { name: "GitHub", href: "https://github.com/pykinsu" },
+    { name: "Community", href: "https://t.me/VVITConnect" },
+    { name: "Twitter", href: "https://twitter.com/pykinsu" },
   ];
 
   return (
@@ -38,70 +29,74 @@ export function MobileNav({ className }: { className?: string }) {
           <Menu className="h-4 w-4" />
         </Button>
       </SheetTrigger>
-      <SheetContent side="left">
-        <h2 className="mb-4 text-center text-lg font-semibold">
-          Site Navigation
-        </h2>
-        <ul className="mb-8 space-y-3">
-          {siteLinks.map((link) => (
-            <li key={link.href}>
-              <Button
-                variant={pathname === link.href ? "default" : "outline"}
-                asChild
-              >
-                <Link
-                  href={link.href}
-                  className="w-full"
-                  onClick={handleCloseSheet}
+
+      <SheetContent side="left" className="p-0">
+        {/* Scrollable content wrapper */}
+        <div className="h-full overflow-y-auto p-6">
+          <h2 className="mb-4 text-center text-lg font-semibold">
+            Legal Links
+          </h2>
+          <ul className="mb-8 space-y-3">
+            {siteLinks.map((link) => (
+              <li key={link.href}>
+                <Button
+                  variant={pathname === link.href ? "default" : "outline"}
+                  asChild
                 >
-                  {link.name}
-                </Link>
-              </Button>
-            </li>
-          ))}
-        </ul>
+                  <Link
+                    href={link.href}
+                    className="w-full"
+                    onClick={handleCloseSheet}
+                  >
+                    {link.name}
+                  </Link>
+                </Button>
+              </li>
+            ))}
+          </ul>
 
-        <Separator className="my-4" />
+          <Separator className="my-4" />
 
-        <h2 className="mb-4 text-center text-lg font-semibold">
-          External Links
-        </h2>
-        <ul className="space-y-3">
-          {externalLinks.map((link) => (
-            <li key={link.href}>
-              <Button variant="outline" asChild>
-                <Link
-                  href={link.href}
-                  className="w-full"
-                  target="_blank"
-                  onClick={handleCloseSheet}
-                >
-                  {link.name}
-                </Link>
-              </Button>
-            </li>
-          ))}
-        </ul>
+          <h2 className="mb-4 text-center text-lg font-semibold">
+            Reach Admin
+          </h2>
+          <ul className="space-y-3">
+            {externalLinks.map((link) => (
+              <li key={link.href}>
+                <Button variant="outline" asChild>
+                  <Link
+                    href={link.href}
+                    className="w-full"
+                    target="_blank"
+                    onClick={handleCloseSheet}
+                  >
+                    {link.name}
+                  </Link>
+                </Button>
+              </li>
+            ))}
+          </ul>
 
-        <Separator className="my-4" />
+          <Separator className="my-4" />
 
-        <h2 className="mb-4 text-center text-lg font-semibold">Socials</h2>
-        <ul className="space-y-3">
-          {socials.map((link) => (
-            <li key={link.href}>
-              <Button variant="outline" asChild>
-                <Link
-                  href={link.href}
-                  className="w-full"
-                  target="_blank"
-                  onClick={handleCloseSheet}
-                >
-                  {link.name}
-                </Link>
-              </Button>
-            </li>
-          ))}
-        </ul>
+          <h2 className="mb-4 text-center text-lg font-semibold">Socials</h2>
+          <ul className="space-y-3">
+            {socials.map((link) => (
+              <li key={link.href}>
+                <Button variant="outline" asChild>
+                  <Link
+                    href={link.href}
+                    className="w-full"
+                    target="_blank"
+                    onClick={handleCloseSheet}
+                  >
+                    {link.name}
+                  </Link>
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </SheetContent>
     </Sheet>
   );
