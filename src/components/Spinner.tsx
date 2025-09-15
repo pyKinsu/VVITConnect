e02ed2fdc5431cb4b3dcdc4408b6a0972140de6a
Kinsu Rajput <kissuxrajput@gmail.com>
2025-09-15 18:14:47 +0530

    Create Spinner.tsx

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,24 @@
+"use client"; // ensures this works in Next.js
+
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="spinner">
+        <div className="spin1 animate-spin">
+          <div className="spin2"></div>
+          <div className="spin3"></div>
+          <div className="spin4"></div>
+          <div className="spin5"></div>
+          <div className="spin6"></div>
+          <div className="spin7"></div>
+          <div className="spin8"></div>
+          <div className="spin9"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
