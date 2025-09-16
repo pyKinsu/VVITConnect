666457cb75acb14cf6fac4b48c6fb5c76d6f3174
Kinsu Rajput <kissuxrajput@gmail.com>
2025-09-16 16:57:44 +0530

    Create subjectOptions.ts

---
+++ src/lib/subjectOptions.ts
@@ -0,0 +1,12 @@
+// lib/subjectOptions.ts
+
+export const subjectOptions: { [key: string]: string } = {
+  PSPC: "Problem Solving Programming Concept",
+  CE: "Communicative English",
+  PMO: "Principles of Management & Organisation",
+  ITA: "Information Technology Application",
+  "ITA Lab": "Information Technology Application Lab",
+  BM: "Business Mathematics",
+  "PSPC Lab": "Problem Solving Programming Concept Lab",
+  "Spt & Cul": "Sports And Cultural Activities",
+};
