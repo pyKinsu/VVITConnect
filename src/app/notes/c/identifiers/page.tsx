c68f4563da228d1a068e5af15cc5e872d5c80df3
Kinsu Rajput <kissuxrajput@gmail.com>
2025-09-16 08:08:19 +0530

    Update page.tsx

--- src/app/notes/c/identifiers/page.tsx
+++ src/app/notes/c/identifiers/page.tsx
@@ -39,7 +39,7 @@ export default function Identifier() {
       </div>
 
       {/* Rules */}
-      <h2 className="text-xl font-semibold text-secondary mb-3">
+      <h2 className="text-2xl font-semibold text-secondary mb-3">
         Rules for Identifiers
       </h2>
       <ul className="list-disc pl-6 space-y-2 text-muted-foreground">
