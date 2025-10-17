b1bdad4d92c387836f4484ab1fa4baecdf9dbdc2
Kinsu Rajput <kissuxrajput@gmail.com>
2025-10-17 22:54:57 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,22 @@ import HomePopup from "@/components/HomePopup"; // default export now
 export default function HomePage() {
   return (
     <div className="flex flex-col py-8">
+      <div className="flex gap-4 mb-4">
+        <Image
+          src="/kids.png"
+          alt="Decorative 1"
+          width={50}
+          height={50}
+          className="object-contain"
+        />
+        <Image
+          src="/admit.png"
+          alt="Decorative 2"
+          width={50}
+          height={50}
+          className="object-contain"
+        />
+      </div>
       <h1 className="text-balance mb-4 text-center text-4xl font-extrabold">
         Choose Your Class Below Or For Contacting Us
       </h1>
